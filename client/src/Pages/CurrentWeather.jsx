1c5bfea4aa5beb1562ff4a006ad6fcbefd582a26
Arun Kumar <arunkumare.dev@gmail.com>
2024-01-26 17:02:00 +0530

    Deplaying

--- client/src/Pages/CurrentWeather.jsx
+++ client/src/Pages/CurrentWeather.jsx
@@ -1,5 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Box } from "@mui/material";
+import axios from "axios";
+import { APP_URL } from "../constants";
 
 export default function CurrentWeather() {
-  return <>Current Weather</>;
+  const [weatherData, setWeatherData] = useState({ data: "Arun" });
+
+  const getCurrentLocation = async () => {
+    return new Promise((resolve, reject) => {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          (position) =>
+            resolve(`${position.coords.latitude},${position.coords.longitude}`),
+          (error) => {
+            console.error("Error getting location:", error.message);
+            reject(error.message);
+          }
+        );
+      } else {
+        console.error("Geolocation is not supported by this browser.");
+        reject("Geolocation is not supported by this browser.");
+      }
+    });
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const location = await getCurrentLocation();
+        console.log("Location:", location);
+
+        const response = await axios.post(`${APP_URL}/currentWeather`, {
+          location: location,
+        });
+
+        console.log(response);
+        setWeatherData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Box>
+      <pre>{JSON.stringify(weatherData, null, 2)}</pre>
+    </Box>
+  );
 }
