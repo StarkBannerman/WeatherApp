cc904d11205603930c73ad3075974d70c6a6931f
Arun Kumar <arunkumare.dev@gmail.com>
2024-03-04 11:51:25 +0530

    Bot completed

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,2 @@
-export const APP_URL = "https://weather-app-nine-gamma-18.vercel.app";
+export const APP_URL = "https://projects.aruncodes.site";
 //export const APP_URL = "https://skunk-capable-penguin.ngrok-free.app";
